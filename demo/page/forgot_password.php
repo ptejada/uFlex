86e598e3149c3f38b7f23cff3da04cbf73934281
Pablo Tejada <ptejada26@gmail.com>
2012-06-18 19:36:30 -0400

    Global demo forms update

--- demo/page/forgot_password.php
+++ demo/page/forgot_password.php
@@ -4,13 +4,13 @@
 		<?php echo showMsg()?>
 	</div>
 
-    <form method="post" action="ps/reset_password.php">
-        <p>Enter the email associated with your account</p>            
-        <label>Email: </label><span class="required">*</span>
-        <input name="email" type="text" value="">
-        
-                
-        <input value="Reset Password" type="submit">
-    </form>
-    
-    <a href="?page=login">Login</a>
+	<form method="post" action="ps/reset_password.php">
+		<p>Enter the email associated with your account</p>			
+		<label>Email: </label><span class="required">*</span>
+		<input name="email" type="text" value="">
+		
+				
+		<input value="Reset Password" type="submit">
+	</form>
+	
+	<a href="?page=login">Login</a>
