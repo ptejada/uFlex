2ef6a39df1a86964bada94337760e5717c824340
Pablo Tejada <ptejada26@gmail.com>
2012-07-23 22:12:22 -0400

    Fixed cookie bug for PHP 5.4.x

--- updater/uFlex.updater.php
+++ updater/uFlex.updater.php
@@ -23,8 +23,8 @@ h2+div {
 		$err = "<h2 class='error'>$err</h2><h1>Ok <a href='{$_SERVER['SCRIPT_NAME']}'>Continue</a></h1>";
 		die($err);
 	}
-	$toVersion = 0.87;
-	$fromVersion = 0.86;
+	$toVersion = 0.88;
+	$fromVersion = 0.87;
 	
 	$classFile = "class.uFlex.php";
 	$template = "class.uFlex.update.tpl";
