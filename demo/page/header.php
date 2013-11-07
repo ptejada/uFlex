350c3ac2b05d6c343ed57bcff94e277b5efff6cf
Pablo Tejada <ptejada26@gmail.com>
2013-11-06 21:07:16 -0500

    Added the missing meta value for proper display on mobile devices

--- demo/page/header.php
+++ demo/page/header.php
@@ -1,8 +1,10 @@
 <html>
 <head>
 	<title><?php echo $pageTitle?> | uFlex</title>
+	
 	<meta name="robots" content="noindex">
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	<link href="http://bootswatch.com/readable/bootstrap.min.css" rel="stylesheet">
 	<link rel=stylesheet type=text/css href="<? echo $base?>/static/css/style.css" />
 
