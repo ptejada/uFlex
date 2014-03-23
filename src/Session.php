605b9d8d66cbc80d80a223d06a0399af4958dcef
Pablo Tejada <ptejada26@gmail.com>
2014-03-23 18:52:43 -0400

    Added a session handler

---
+++ src/Session.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ptejada\UFlex;
+
+/**
+ * Class to handle the PHP session
+ * The entire PHP session can be handled
+ * Or a direct member of the main session array
+ * which is refer as a namespace
+ *
+ * @package Ptejada\UFlex
+ */
+class Session extends Collection
+{
+    /** @var  Log - Log errors and report */
+    public $log;
+
+    /**
+     * Initialize a session handler by namespace
+     *
+     * @param string $namespace - Session namespace to manage
+     */
+    public function __construct($namespace = null)
+    {
+        $this->log = new Log('Session');
+
+        // Starts the session if it has not been started yet
+        if (!isset($_SESSION) && !headers_sent()) {
+            session_start();
+            $this->log->report("Session is been started...");
+        } elseif (isset($_SESSION)) {
+            $this->log->report("Session has already been started");
+        } else {
+            $this->log->error("Session could not be started");
+        }
+
+        if (is_null($namespace)) {
+            // Manage the whole session
+            parent::__construct($_SESSION);
+        } else {
+            if (!isset($_SESSION[$namespace])) {
+                // Initialize the session namespace if does not exists yet
+                $_SESSION[$namespace] = array();
+            }
+
+            // Link the SESSION namespace to the local $data variable
+            parent::__construct($_SESSION[$namespace]);
+        }
+
+    }
+}
---
+++ tests/SessionTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pablo
+ * Date: 3/23/14
+ * Time: 5:58 PM
+ */
+
+namespace tests;
+
+
+use Ptejada\UFlex\Session;
+
+class SessionTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp()
+    {
+        // Initializes the global session array
+        $_SESSION = array();
+    }
+    public function testInitialization()
+    {
+        // Manage the whole session
+        $session = new Session();
+        $this->assertEquals(array(), $session->getAll());
+        $this->assertFalse($session->log->hasError());
+
+        //Manage namespace on the SESSION
+        $this->assertArrayNotHasKey('test', $_SESSION, 'The session test namespace should not exists');
+        $session = new Session('test');
+        $this->assertArrayHasKey('test', $_SESSION);
+        $this->assertFalse($session->log->hasError());
+    }
+
+    public function testSettersAndGetters()
+    {
+        $session = new Session('test');
+        $session->test = 'Hello World';
+        $this->assertEquals('Hello World', $session->test);
+        $this->assertEquals('Hello World', $_SESSION['test']['test']);
+
+        // Tests chaining lists
+        $session->list = array();
+        $session->list->one = 1;
+        $session->list->two = 2;
+
+        $this->assertEquals($_SESSION['test']['list'], $session->list->getAll());
+    }
+
+}
+ 
